4e9aec11b5196dcad796be1027d256897889677e
Bernard Cafarelli <voyageur@gentoo.org>
2007-05-10 08:40:12 +0000

    Moved gnustep work to gnustep overlay
    git-svn-id: file:///var/svn/repos/voyageur-overlay@196 0adde87b-6219-0410-b567-eaaea72d81a5

--- gnustep-base/gnustep-back-cairo/files/config-gnustep-back-cairo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Switching to libgnustep-cairo backend ..."
-echo "defaults write NSGlobalDomain GSBackend libgnustep-cairo"
-defaults write NSGlobalDomain GSBackend libgnustep-cairo
-
